60feb052ed50606b47b6c87732a2080d8066d360
Paul Golovatyuk <decapitator1988@gmail.com>
2018-03-14 18:21:05 +0200

    some refactoring...

--- src/anagram/Application.java
+++ src/anagram/Application.java
@@ -1,21 +1,20 @@
-import java.util.Arrays;
+package anagram;
 
 public class Application {
-    static String lettersOnly = "abcd efgh";
-    static String notLettersOnly = "a1bcd efg!h";
+    private static String lettersOnly = "Messi7 abcd efgh";
+    private static String notLettersOnly = "a1bcd efg!h";
 
     public static void main(String[] args) {
 
-
         System.out.println("Reversing strings with letters only demo: ");
         System.out.println("Before:     " + lettersOnly);
-        System.out.println("After:      " + Helper.reverseLettersOnly(lettersOnly));
+        System.out.println("After:      " + StringReverser.reverseStrings(lettersOnly));
 
         System.out.println();
 
         System.out.println("Reversing strings with not letters only demo: ");
         System.out.println("Before:     " + notLettersOnly);
-        System.out.println("After:      " + Helper.reverseNotLettersOnly(notLettersOnly));
+        System.out.println("After:      " + StringReverser.reverseStrings(notLettersOnly));
 
     }
 }
---
+++ src/anagram/StringReverser.java
@@ -0,0 +1,48 @@
+package anagram;
+
+public class StringReverser {
+
+    private static String[] splitByWhiteSpace(String s) {
+        return s.split(" ");
+    }
+
+    private static boolean isLetter(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            return true;
+        }
+        return false;
+    }
+
+    private static char[] reverseChars(char[] chars1) {
+        char tmp;
+        for (int i = 0, j = chars1.length - 1; i < chars1.length - 1 && j > i; ) {
+            if (isLetter(chars1[i]) && isLetter(chars1[j])) {
+                tmp = chars1[i];
+                chars1[i] = chars1[j];
+                chars1[j] = tmp;
+                i++;
+                j--;
+            }
+            if (!isLetter(chars1[i])) {
+                i++;
+            }
+            if (!isLetter(chars1[j])) {
+                j--;
+            }
+        }
+        return chars1;
+    }
+    public static String reverseStrings(String s) {
+        String[] strings = splitByWhiteSpace(s);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i <strings.length ; i++) {
+                char[] chars = strings[i].toCharArray();
+                String str = new String(reverseChars(chars));
+                sb.append(str+" ");
+        }
+        return sb.toString();
+    }
+}
+
+
