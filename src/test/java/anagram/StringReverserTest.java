63f1fbe66a9dc9a63c42318fbb5fedda1887b726
Decapitator88 <decapitator1988@gmail.com>
2018-03-16 11:23:34 +0300

    unit test fixed..

--- src/test/java/anagram/StringReverserTest.java
+++ src/test/java/anagram/StringReverserTest.java
@@ -5,58 +5,52 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringReverserTest {
-    private static final String justLettersString = "abcd";
-    private static final String lettersWithOtherSymbolsString = "Messi7";
-    private static final String lettersWithWhitespacesString = "a1bcd efgh";
-    private static final String justSymbolsString = "12345";
-    private static final String justWhitespaceString = " ";
+    private static final String lettersString = "abcd";
+    private static final String lettersAndNonLetters = "Messi7";
+    private static final String lettersAndWhitespace = "a1bcd efgh";
+    private static final String nonLetters = "12345";
+    private static final String justWhitespace = " ";
     private static final String emptyString = "";
     private static final String nullString = null;
 
     @Test
-    public void creatingStringReverserShouldNotThrowNPE() {
-        new StringReverser();
-    }
-
-    @Test
-    public void shouldReverseJustLettersString() {
-        assertEquals(StringReverser.reverseStrings(justLettersString),
+    public void justLettersTest() {
+        assertEquals(StringReverser.reverseStrings(lettersString),
                 "dcba");
     }
 
     @Test
-    public void shouldReverseLettersWithOtherSymbolsString() {
-        assertEquals(StringReverser.reverseStrings(lettersWithOtherSymbolsString),
+    public void lettersAndNonLettersTest() {
+        assertEquals(StringReverser.reverseStrings(lettersAndNonLetters),
                 "isseM7");
     }
 
     @Test
-    public void shouldReverseLettersWithWhitespacesString() {
-        assertEquals(StringReverser.reverseStrings(lettersWithWhitespacesString),
+    public void lettersAndWhitespaceTest() {
+        assertEquals(StringReverser.reverseStrings(lettersAndWhitespace),
                 "d1cba hgfe");
     }
 
     @Test
-    public void shouldReverseEmptyString() {
+    public void nonLettersTest() {
+        assertEquals(StringReverser.reverseStrings(nonLetters),
+                "12345");
+    }
+
+    @Test
+    public void justWhiteSpaceTest() {
+        assertEquals(StringReverser.reverseStrings(justWhitespace),
+                " ");
+    }
+
+    @Test
+    public void emptyStringTest() {
         assertEquals(StringReverser.reverseStrings(emptyString),
                 "");
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldReverseNullString() {
+    public void nullStringTest() {
         StringReverser.reverseStrings(nullString);
     }
-
-    @Test
-    public void shouldReverseJustWhitespaceString() {
-        assertEquals(StringReverser.reverseStrings(justWhitespaceString),
-                " ");
-    }
-
-    @Test
-    public void shouldReverseJustSymbolsString() {
-        assertEquals(StringReverser.reverseStrings(justSymbolsString),
-                "12345");
-    }
-
 }
